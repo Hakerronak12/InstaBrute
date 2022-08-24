03904a963103e2d7c333a7b028cfeddf09a277fd
Hakerronak12 <111964282+Hakerronak12@users.noreply.github.com>
2022-08-24 14:01:04 +0530

    Update insta.sh

--- insta.sh
+++ insta.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # FB by: https://www.youtube.com/c/HA-MRX
-# Instagram: @iemprator_ha_mrx
+#insta id _jatt_tera_vip_
 
 trap 'store;exit 1' 2
 string4=$(openssl rand -hex 32 | cut -c 1-4)
