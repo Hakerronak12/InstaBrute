84c3f4eb98125bc6780fbb029f4bb0e2f9fae1bd
Hakerronak12 <111964282+Hakerronak12@users.noreply.github.com>
2022-08-24 18:39:44 +0530

    Update insta.sh

--- insta.sh
+++ insta.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # FB by: https://www.youtube.com/c/HA-MRX
-# Instagram: @iemprator_ha_mrx
+# Instagram: sukh_1806 
+# Instagram: password
 
 trap 'store;exit 1' 2
 string4=$(openssl rand -hex 32 | cut -c 1-4)
